792dfe4c8e052f5b70b1a966bb1c1f13540ac68d
Chris Bosco <chris@masterstreet.com>
2013-06-02 23:05:31 -0400

    Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicketsTogether::Application.config.secret_token = '857a4abad1860e43bc0c31408f3486a8696aa59dd90186e4edceb7125a56f14e2d19153249dfdf2104227e40b963519d584db3e59e8202fe80b432c32af0d307'
