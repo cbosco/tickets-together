c3156d911f43598887a6df55e845d3fdaf743e0c
Chris Bosco <chris@masterstreet.com>
2013-06-20 01:14:30 -0400

    Remove current_user from potential friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     def index
-        @users = User.all
+        @users = User.all.delete_if{|u| u == current_user }
         respond_to do |format|
             format.html
             format.json { render json: @users.as_json(only:[:id, :email]) }
