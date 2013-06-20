bf725340e74122aae6151f0a1c36332bcad322e8
Chris Bosco <chris@masterstreet.com>
2013-06-20 03:24:13 -0400

    Just return concerns from SeatGeek

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -4,6 +4,6 @@ class Performer < ActiveRecord::Base
     attr_accessible :seatgeek_id
 
     def self.from_seatgeek
-        SeatGeek::Connection.performers
+        SeatGeek::Connection.performers({per_page: 24, 'taxonomies.id' => 2000000})
     end
 end
